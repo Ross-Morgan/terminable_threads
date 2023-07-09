1ea5e763c4ceff070dd389d6eeddd50c32c18936
Ross Morgan <morgan.ross.512@gmail.com>
2023-07-09 16:35:18 +0100

    add prelude and traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,15 @@
 pub mod group;
 pub mod single;
+pub mod traits;
+
+pub use single::*;
+pub use group::*;
+pub use traits::*;
+
+pub mod prelude {
+    use super::*;
+
+    pub use single::*;
+    pub use group::*;
+    pub use traits::*;
+}
