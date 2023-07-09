19c4e1d3880f3405e80ffe1a2f2dfff7dfb8cc73
Ross Morgan <morgan.ross.512@gmail.com>
2023-07-09 16:35:35 +0100

    impl Join, Terminate for TerminableTheradHandle

--- src/single.rs
+++ src/single.rs
@@ -1,35 +1,49 @@
+use std::any::Any;
+use std::marker::PhantomData;
+use std::sync::atomic::{self, AtomicBool};
+use std::sync::{Arc, Mutex};
+use std::thread::JoinHandle;
+
+use super::traits::{Join, Terminate};
 
 
+/// A thread type that can terminate early using an atomic bool flag
+/// The provided function must take and use this flag to be terminable
 pub struct TerminableThreadHandle<T, F>
 where
-    F: FnOnce(Arc<AtomicBool>) -> T,
+    F: FnOnce(Arc<Mutex<AtomicBool>>) -> T,
     F: Send + 'static,
     T: Send + 'static,
 {
-    /// Thread-safe bool, signalling whether the function should terminate
-    running: Arc<AtomicBool>,
-
-    inner_thread: JoinHandle<T>,
-
-    _marker: PhantomData<F>
+    pub(crate) running: Arc<Mutex<AtomicBool>>,
+    pub(crate) inner_thread: JoinHandle<T>,
+    pub(crate) _marker: PhantomData<F>,
 }
 
 
-impl<T, F> TerminableThreadHandle<T, F>
+impl<T, F> Join<T> for TerminableThreadHandle<T, F>
 where
-    F: Fn(Arc<AtomicBool>) -> T,
+    F: FnOnce(Arc<Mutex<AtomicBool>>) -> T,
     F: Send + 'static,
     T: Send + 'static,
 {
-    pub fn spawn(f: F) -> Self
-    {
-        let arc_bool = Arc::new(AtomicBool::new(true));
+    fn join(self) -> Result<T, Box<dyn Any + Send + 'static>> {
+        self.inner_thread.join()
+    }
+}
 
 
-        Self {
-            running: Arc::clone(&arc_bool),
-            inner_thread: thread::spawn(move || f(arc_bool)),
-            _marker: PhantomData,
-        }
+impl<T, F> Terminate for TerminableThreadHandle<T, F>
+where
+    F: FnOnce(Arc<Mutex<AtomicBool>>) -> T,
+    F: Send + 'static,
+    T: Send + 'static,
+{
+    fn terminate(&self) {
+        // TODO: Get rid of panic
+        match self.running.lock() {
+            Ok(b) => b.store(false, atomic::Ordering::SeqCst),
+            Err(_) => panic!("Couldn't terminate terminable thread due to mutex already being locked by current thread"),
+        };
     }
 }
