11dcd12847ed02153f2287f8f53a2c3a9047b8e1
Ross Morgan <morgan.ross.512@gmail.com>
2023-07-09 16:35:08 +0100

    change flag to arc mutex

--- src/group.rs
+++ src/group.rs
@@ -1,36 +1,41 @@
-use std::{sync::{atomic::AtomicBool, Arc}, thread::{self, JoinHandle}, marker::PhantomData};
+use std::marker::PhantomData;
+use std::sync::atomic::AtomicBool;
+use std::sync::{Arc, Mutex};
+use std::thread::JoinHandle;
 
-/// A thread that can be terminated
+/// A group of threads that can be terminated early using an atomic bool flag
+/// The provided functions must take and use this flag to be terminable
 
-
-
-/// A group of threads that can be terminated
-///
 pub struct TerminableThreadGroup<T, F>
 where
-    F: FnOnce(Arc<AtomicBool>) -> T,
+    F: FnOnce(Arc<Mutex<AtomicBool>>) -> T,
     F: Send + 'static,
     T: Send + 'static,
 {
     /// Thread-safe bool, signalling whether the functions should terminate
-    running: Arc<AtomicBool>,
+    running: Arc<Mutex<AtomicBool>>,
 
     /// Vec of functions or closures that take an atomic bool as an argument
     inner_threads: Vec<JoinHandle<T>>,
 
+    _marker: PhantomData<F>,
+}
+
+/// A group of threads that can be terminated early using an atomic bool flag
+/// The provided functions must take and use this flag to be terminable
+///
+/// This uses a fixed size array instead of a vec
+pub struct TerminableThreadGroupArray<T, F, const N: usize>
+where
+    F: Fn(Arc<Mutex<AtomicBool>>) -> T,
+{
+    /// Thead-safe bool, signalling whether the functions should terminate
+    running: Arc<Mutex<AtomicBool>>,
+
+    /// Array of terminable threads
+    inner_threads: [JoinHandle<T>; N],
 
     _marker: PhantomData<F>,
 }
 
-/// A group of threads that can be terminated
-/// This uses a fixed size array instead of a vec
-pub struct TerminableThreadGroupArray<T, F, const N: usize>
-where
-    F: Fn(Arc<AtomicBool>) -> T,
-{
-    /// Thead-safe bool, signalling whether the functions should terminate
-    running: Arc<AtomicBool>,
-
-    /// Array of terminable threads
-    inner_threads: [F; N],
-}
+// TODO: impl `Join<T>`, `Terminate` for {`TerminableThreadGroup`, `TerminableThreadGroupArray`}
