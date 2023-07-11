22088039160ba52ce2b87cf7a2db28cd1e57416b
Ross-Morgan <rmorgan512@protonmail.ch>
2023-07-11 23:39:14 +0100

    formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 use std::any::Any;
 use std::fmt::Debug;
 use std::marker::PhantomData;
-use std::sync::atomic::{AtomicBool, self};
+use std::sync::atomic::{self, AtomicBool};
 use std::sync::{Arc, Mutex};
 use std::thread::JoinHandle;
 
 /// A basic thread manager that can signal all threads to terminate / finish early
-/// 
+///
 /// Note that threads will only terminate if the `Ref<Mutex<AtomicBool>>` flag is used
 #[derive(Debug)]
 pub struct TerminableThreads<T, const N: usize> {
@@ -20,29 +20,35 @@ impl<T, const N: usize> TerminableThreads<T, N> {
     }
 
     /// Signal all threads to terminate and cease operation
-    /// 
+    ///
     /// ## Note
-    /// 
+    ///
     /// This does not guarantee all threads will terminate, or can be terminated.
-    /// 
+    ///
     /// Threads will only terminate if the underlying function checks the flag passed to it.s
     pub fn terminate(&self) {
-        self._terminate_flag.lock().expect("Failed to lock termination flag").store(true, atomic::Ordering::SeqCst);
+        self._terminate_flag
+            .lock()
+            .expect("Failed to lock termination flag")
+            .store(true, atomic::Ordering::SeqCst);
     }
 
     /// Join all threads, optionally signalling termination
-    /// 
+    ///
     /// Optional termination signalling is useful because no termination signal
     /// will let allow a function to run until it has finished naturally, while
     /// early termination could stop operations earlier than wanted
-    /// 
+    ///
     /// # Returns
-    /// 
+    ///
     /// `[T; N]` being the results of each thread.
-    /// 
+    ///
     /// `[Option<Error>; N]` containing only the errors from each thread that
     /// caused an error
-    pub fn join(self, signal_terminate: bool) -> Result<[T; N], [Option<Box<dyn Any + Send + 'static>>; N]> {
+    pub fn join(
+        self,
+        signal_terminate: bool,
+    ) -> Result<[T; N], [Option<Box<dyn Any + Send + 'static>>; N]> {
         if signal_terminate {
             self.terminate();
         }
@@ -56,20 +62,20 @@ impl<T, const N: usize> TerminableThreads<T, N> {
 
             Ok(res)
         } else {
-            let res = joined.map(|r| match r {
+            let res = joined
+                .map(|r| match r {
                     Ok(a) => Err(a),
                     Err(b) => Ok(Some(b)),
-            })
-            .map(|r| r.unwrap_or(None));
+                })
+                .map(|r| r.unwrap_or(None));
 
             Err(res)
         }
     }
 }
 
-
 /// Basic builder for a terminable thread object
-/// 
+///
 /// The builder is necessary to provide the termination flag (`Arc<Mutex<AtomicBool>>`)
 /// for threads, that are later provided to the builder, to use.
 #[derive(Debug)]
@@ -96,7 +102,7 @@ impl<T, const N: usize> TerminableThreadsBuilder<T, N> {
     pub fn build_with_threads(self, threads: [JoinHandle<T>; N]) -> TerminableThreads<T, N> {
         TerminableThreads {
             _terminate_flag: self.terminate_flag,
-            _threads: threads
+            _threads: threads,
         }
     }
 }
